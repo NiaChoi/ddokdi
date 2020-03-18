bee57863f16017a485b531f784ef1da35b152e46
unsong0316 <uschoe1994@gmail.com>
2020-03-18 17:50:47 +0900

    admin page 업뎃

---
+++ src/switch.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
+import MsgProcessor from "./servepart/MsgProcessor"
+import './App.css';
+
+
+export default function SwitchLabels() {
+  const [state, setState] = React.useState({
+    checkedA: true,
+    checkedB: true,
+  });
+
+  const handleChange = name => event => {
+    setState({ ...state, [name]: event.target.checked });
+  };
+
+  return (
+    <FormGroup row>
+      <FormControlLabel
+        control={
+          <Switch checked={state.checkedA} onChange={handleChange('checkedA')} value="checkedA" />
+          //< Switch value = " checkedA " inputProps = { { 'aria-label' : 'Switch A' } } /> 
+        }
+        
+      />
+      
+    </FormGroup>
+  );
+}
+
+class Client_Emergency_service extends Component {
+  constructor(props){
+    super(props);
+    this.max_content_id = 3;//UI에 영향을 주지 않으므로 state X
+    this.state = {
+      ClientUSERID:0,
+      Activate_Emergency_Service_list:[],
+      Activate_Emergency_Service_length:0
+
+      
+      }
+    }
+    handleEmergencyServiceSubmit_for_activate = event => {
+      // event.preventDefault();
+      console.log(event);
+      let userId = localStorage.getItem("USN");
+      let msgProc = new MsgProcessor();
+      let Client_userId_for_detail = this.state.ClientUSERID; 
+        msgProc.attemptClientEmergencyServiceUpdate_1(userId, Client_userId_for_detail, (result)=> { 
+          if (result[0] == 0) {
+            // alert("응급상황서비스 활성화.");
+            console.log(result[1]);
+          }
+          else {
+            alert(result[1]);
+       }
+    });
+  }
+    handleEmergencyServiceSubmit_for_deactivate = event => {
+      // event.preventDefault();
+      console.log(event);
+      let userId = localStorage.getItem("USN");
+      let msgProc = new MsgProcessor();
+      let Client_userId_for_detail = this.state.ClientUSERID; 
+        msgProc.attemptClientEmergencyServiceUpdate_0(userId, Client_userId_for_detail, (result)=> { 
+          if (result[0] == 0) {
+            // alert("응급상황서비스 비활성화.");
+            console.log(result[1]);
+          }
+          else {
+            alert(result[1]);
+      }
+    });
+  }
+  
+ }
